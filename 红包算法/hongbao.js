ce6d4266be146d1f0ddddab76ea456bdffd47a0f
waihoyu <13701343809@qq.com>
2018-05-07 17:49:12 +0800

    '算法更新'

--- 红包算法/hongbao.js
+++ 红包算法/hongbao.js
@@ -11,15 +11,33 @@ class RadomHongbao {
         this.calcNum = this.num * Math.pow(10, this.multiple);
     }
     split(n, precision) {
-        // this.num
-        console.log(this.calcNum);
-        console.log(this.calcNum % n + 'test');
         let arr = this.average(n, precision);
-        console.log(arr);
+        let arrResult = arr;
+        for (let i = 0; i < arr.length; i++) {
+            let item = arr[i];
+            let num = Math.floor(Math.random() * item);
+            let numLeft = Math.floor(Math.random() * num);
+            let numRight = num - numLeft;
+            arrResult[i] -= num;
+            let iLeft = (i === 0) ? (arr.length - 1) : i - 1;
+            let iRight = (i === arr.length - 1) ? 0 : i + 1;
+            arrResult[iLeft] += numLeft;
+            arrResult[iRight] += numRight;
+        }
+        return arrResult;
     }
     average(n, precision) {
         let avg = Math.floor(this.calcNum / n);
+        let rest = this.calcNum % n;
+        console.log(rest);
         let result = Array(n).fill(avg);
+        let gap = Math.round((n - rest) / rest) + 1;
+        let index = 0;
+        while (rest > 0) {
+            index = (--rest) * gap;
+            result[index >= n ? (n - 1) : index]++;
+        }
+
         return result.map((item) => {
             return (item / Math.pow(10, this.multiple))
         });
@@ -27,13 +45,14 @@ class RadomHongbao {
     getNum(num, defaultNum = 0) {
         return this.isNumber(num) ? num : defaultNum;
     }
-
     isNumber(num) {
         //各种可能出现问题的，排除
         let number = +num; //加号 的作用  
-        console.log(number);
+        if ((number - num) !== 0) {
+            return false;
+        }
         if (number === num) {
-
+            return true;
         }
         if (typeof num === 'string') {
             return false;
@@ -42,6 +61,5 @@ class RadomHongbao {
     }
 }
 
-// 200 => 总金额
-const hongbao = new RadomHongbao(100.12);
-hongbao.split(26, 2);
+const hongbao = new RadomHongbao(200);
+console.log(hongbao.split(10, 0));
